7a6de08e8e0460c0e1d4c2b0e8d45ba6d940ccb3
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-01-31 19:20:31 +0800

    Make node main program loop for user enter address until it connect

--- src/node/go.mod
+++ src/node/go.mod
@@ -1,3 +1,10 @@
 module verteilen2_node
 
 go 1.24.2
+
+require github.com/zhouhui8915/go-socket.io-client v0.0.0-20200925034401-83ee73793ba4
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/zhouhui8915/engine.io-go v0.0.0-20150910083302-02ea08f0971f // indirect
+)
--- src/node/main.go
+++ src/node/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	socketio_client "github.com/zhouhui8915/go-socket.io-client"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var uri string
+	var pass bool = false
+	var client *socketio_client.Client
+
+	opts := &socketio_client.Options{
+		Transport: "websocket",
+		Query:     make(map[string]string),
+	}
+
+	fmt.Print("Welcome be verilen2 node.\n")
+
+	for !pass {
+		fmt.Print("Please enter your server address: ")
+		fmt.Scanln(&uri)
+		fmt.Printf("Trying to connect to server: %s\n", uri)
+
+		buffer, err := socketio_client.NewClient(uri, opts)
+		if err != nil {
+			log.Printf("Failed to create client: %v\n", err)
+			continue
+		}
+		client = buffer
+		pass = true
+	}
+
+	// Handle connection event
+	client.On("connection", func() {
+		log.Println("Connected to server")
+	})
+
+	// Handle custom events from server
+	client.On("message", func(msg string) {
+		log.Printf("Received message: %s", msg)
+	})
+
+	client.On("notification", func(data map[string]interface{}) {
+		log.Printf("Received notification: %v", data)
+	})
+
+	// Handle disconnection
+	client.On("disconnection", func() {
+		log.Println("Disconnected from server")
+	})
+
+	// Handle errors
+	client.On("error", func(err error) {
+		log.Printf("Error: %v", err)
+	})
 }
