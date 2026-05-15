e7ed571c63fc513308cb78279e0a3b8558cb4842
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-05-15 22:48:12 +0800

    Add lib

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,5 +1,13 @@
-#pragma once
+#include <iostream>
+#include "argh.h"
 
-int main(){
-    return 0;
+int main(int argc, char* argv[])
+{
+    argh::parser cmdl;
+    cmdl.parse(argc, argv, argh::parser::PREFER_PARAM_FOR_UNREG_OPTION);
+
+    if (cmdl[{ "-v", "--verbose" }])
+        std::cout << "Verbose, I am.\n";
+
+    return EXIT_SUCCESS;
 }
