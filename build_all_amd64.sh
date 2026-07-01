df6f3271b913883caad309cf18c3d962a30a5c0a
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-07-01 16:14:34 +0800

    Change margin issue for client page

--- build_all_amd64.sh
+++ build_all_amd64.sh
@@ -4,10 +4,4 @@ source setup_all_amd64.sh
 
 cmake --build . -j 8
 
-mkdir -p ../bin/static/master
-mkdir -p ../bin/static/server
-mkdir -p ../bin/static/client
-
-cp -r ../src/master/static/* ../bin/static/master/
-cp -r ../src/server/static/* ../bin/static/server/
-cp -r ../src/client/static/* ../bin/static/client/
+source copy_all_resource.sh
