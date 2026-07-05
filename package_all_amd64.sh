fae18119d2e3573e186002a7688ae63da5726539
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-07-05 16:29:12 +0800

    Update package script

--- package_all_amd64.sh
+++ package_all_amd64.sh
@@ -14,17 +14,25 @@ mkdir -p package/verteilen-2-client/DEBIAN
 mkdir -p package/verteilen-2-client/usr/bin
 mkdir -p package/verteilen-2-client/usr/share
 
+mkdir -p package/verteilen-2-server/DEBIAN
+mkdir -p package/verteilen-2-server/usr/bin
+mkdir -p package/verteilen-2-client/usr/share
+
+mkdir -p package/verteilen-2-master/DEBIAN
+mkdir -p package/verteilen-2-master/usr/bin
+mkdir -p package/verteilen-2-master/usr/share
+
 sed "s|%VV%|$version|g" package/deb_client > package/verteilen-2-client/DEBIAN/control
-cp bin/client package/verteilen-2-client/usr/bin/verteilen-2-client
-cp -r bin/share/verteilen-2-client package/verteilen-2-client/usr/share/verteilen-2-client
+cp bin/bin/client package/verteilen-2-client/usr/bin/verteilen-2-client
+cp -r bin/bin/share/verteilen-2-client package/verteilen-2-client/usr/share/verteilen-2-client
 
 sed "s|%VV%|$version|g" package/deb_server > package/verteilen-2-server/DEBIAN/control
-cp bin/server package/verteilen-2-server/usr/bin/verteilen-2-server
-cp -r bin/share/verteilen-2-server package/verteilen-2-server/usr/share/verteilen-2-server
+cp bin/bin/server package/verteilen-2-server/usr/bin/verteilen-2-server
+cp -r bin/bin/share/verteilen-2-server package/verteilen-2-server/usr/share/verteilen-2-server
 
 sed "s|%VV%|$version|g" package/deb_master > package/verteilen-2-master/DEBIAN/control
-cp bin/master package/verteilen-2-master/usr/bin/verteilen-2-master
-cp -r bin/share/verteilen-2-master package/verteilen-2-master/usr/share/verteilen-2-master
+cp bin/bin/master package/verteilen-2-master/usr/bin/verteilen-2-master
+cp -r bin/bin/share/verteilen-2-master package/verteilen-2-master/usr/share/verteilen-2-master
 
 cd package
 
