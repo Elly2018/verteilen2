c809df3f1c1818ecce016d0f1aa99c8899a01e0b
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-07-17 22:46:06 +0800

    Update build scripts

--- scripts/native/setup.sh
+++ scripts/native/setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-echo $@
-exit 0
+MODULE_CHOICE="$1"
 
 echo "Start setup the cmake project"
 
@@ -11,4 +10,28 @@ mkdir -p build/native
 
 cd build/native
 
-cmake ../..
+if [[ "${MODULE_CHOICE}" == "all" ]]; then
+    args="-D ENABLE_CLI=ON -D ENABLE_CLIENT=ON -D ENABLE_COMMON=ON -D ENABLE_MASTER=ON -D ENABLE_SERVER=ON"
+fi
+
+if [[ "${MODULE_CHOICE}" == "cli" ]]; then
+    args="-D ENABLE_CLI=ON"
+fi
+
+if [[ "${MODULE_CHOICE}" == "client" ]]; then
+    args="-D ENABLE_CLIENT=ON"
+fi
+
+if [[ "${MODULE_CHOICE}" == "common" ]]; then
+    args="-D ENABLE_COMMON=ON"
+fi
+
+if [[ "${MODULE_CHOICE}" == "master" ]]; then
+    args="-D ENABLE_MASTER=ON"
+fi
+
+if [[ "${MODULE_CHOICE}" == "server" ]]; then
+    args="-D ENABLE_SERVER=ON"
+fi
+
+cmake ../.. ${args}
